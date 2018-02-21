cf38abb42bf71dd82dca5a071e02380ebdd65c60
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-02-21 05:46:21 -0400

    removing verbose

--- volatility.py
+++ volatility.py
@@ -15,42 +15,23 @@ PRICE = "CLOSE"
 VOLUME = "VOLUME"
 
 # load data pairs
-print
-print "loading pairs...",
 pairs = get_mt_pairs()
-print "done"
-print
 
-try:
-    raise IOError
-    historical_volatility = pickle.load(open("historical_volatility.p", "rb"))
-    print "loaded volatility"
-    print
-except IOError:
-    print "computing volatility for pairs..."
-    historical_volatility = OrderedDict()
-    for symbol in pairs:
-        print symbol
-        time = pairs[symbol].index.values
-        price = pairs[symbol][PRICE]
-        binned_time = pd.cut(time, bins=time[::TIME_FRAME])
-        # print binned_time
-        binned_pair = price.groupby(binned_time)
+historical_volatility = OrderedDict()
+for symbol in pairs:
+    time = pairs[symbol].index.values
+    price = pairs[symbol][PRICE]
+    binned_time = pd.cut(time, bins=time[::TIME_FRAME])
+    # print binned_time
+    binned_pair = price.groupby(binned_time)
 
-        historical_volatility[symbol] = []
-        for _bin in sorted(binned_pair.groups):
-            sample = binned_pair.get_group(_bin)
-            mean = np.mean(sample)
-            stdd = np.std(sample)
+    historical_volatility[symbol] = []
+    for _bin in sorted(binned_pair.groups):
+        sample = binned_pair.get_group(_bin)
+        mean = np.mean(sample)
+        stdd = np.std(sample)
 
-            historical_volatility[symbol] += [stdd / mean * 100.0]
-    print "...done"
-    print
-    # print "saving historical_volatility...",
-    # pickle.dump(historical_volatility, open("historical_volatility.p", "wb"))
-    # print "done"
-    # print
-
+        historical_volatility[symbol] += [stdd / mean * 100.0]
 
 recent_volatility = OrderedDict()
 for symbol in historical_volatility:
