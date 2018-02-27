651f0af614a24d80cef4934b69e3d61039e9f737
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-02-27 15:37:17 -0400

    fixed some bugs

--- pyCBT/data/providers/oanda/account.py
+++ pyCBT/data/providers/oanda/account.py
@@ -1,32 +1,40 @@
 import os, yaml
 from pyCBT.constants import DATADIR
 from collections import OrderedDict
+import oandapyV20
+from oandapyV20.endpoints.accounts import AccountList
 
+ATTRS = OrderedDict(
+    environment=dict(default="practice", help="Server environment", choices=["practice", "live"]),
+    port=dict(type=int, default=443, help="Server port"),
+    ssl=dict(type=bool, default=True, help="Use SSL protocol?"),
+    token=dict(help="API authorization token"),
+    username=dict(help="Username of the account owner"),
+    datetime_format=dict(default="RFC3339", help="Datetime format", choices=["RFC3339", "UNIX"]),
+    accounts=dict(help="List of accounts owned by user"),
+    active_account=dict(help="Active account")
+)
 
 class Config(object):
-    def __init__(self, new=False, interactive=False):
+    def __init__(self, new=False, interactive=False, **cmd_kwargs):
 
-        self.CONF_FILE = os.path.join(DATADIR, "providers/oanda/.oanda-account-{}.yml")
-        self.account_attrs = OrderedDict(
-            hostname=dict(default="api-fxpractice.oanda.com", help="Server hostname"),
-            streaming_hostname=dict(default="stream-fxpractice.oanda.com", help="Streaming server hostname"),
-            port=dict(type=int, default=443, help="Server port"),
-            ssl=dict(type=bool, default=True, help="Use SSL protocol?"),
-            token=dict(help="API authorization token"),
-            username=dict(help="Username of the account owner"),
-            datetime_format=dict(default="RFC3339", help="Datetime format", choices=["RFC3339", "UNIX"]),
-            accounts=dict(help="List of accounts owned by user"),
-            active_account=dict(help="Active account")
-        )
-        # ask for hostname
-        self.hostname = self.ask_plain(
-            header=self.account_attrs["hostname"]["help"],
-            default=self.account_attrs["hostname"]["default"]
+        # use kwargs to set default values in self.account_attrs
+        self.account_attrs = ATTRS
+        for attr in cmd_kwargs:
+            self.account_attrs[attr]["default"] = cmd_kwargs.get(attr)
+        self.FILENAME = os.path.join(DATADIR, "providers/oanda/.oanda-account-{}.yml")
+        # ask for environment (URL, choices: practice, live)
+        self.environment = self.ask_choice(
+            header="Available environments",
+            choices=self.account_attrs["environment"]["choices"],
+            default=self.account_attrs["environment"]["default"],
+            question=self.account_attrs["environment"]["help"]
+
         )
         # ask for token
         self.token = self.ask_plain(header=self.account_attrs["token"]["help"])
         # get accounts
-        self.accounts = self.get_accounts(self.token)
+        self.accounts = self.get_accounts(self.token, self.environment)
         # ask for active account
         self.active_account = self.ask_choice(
             header="Available accounts",
@@ -34,17 +42,13 @@ class Config(object):
             question=self.account_attrs["active_account"]["help"]
         )
         # if config file exist and not new
-        self.config_file = self.CONF_FILE.format(self.active_account)
-        if os.path.isfile(self.config_file) and not new:
+        self.filename = self.FILENAME.format(self.active_account)
+        if os.path.isfile(self.filename) and not new:
         #   read account info
-            self.from_file = self.get_from_file(self.config_file)
+            self.from_file = self.get_from_file(self.filename)
         #   if interactive
             if interactive:
         #       ask for rest of parameters using config file as default values
-                self.streaming_hostname = self.ask_plain(
-                    header=self.account_attrs["streaming_hostname"]["help"],
-                    default=self.from_file["streaming_hostname"]
-                )
                 self.port = self.ask_plain(
                     header=self.account_attrs["port"]["help"],
                     default=self.from_file["port"],
@@ -65,13 +69,16 @@ class Config(object):
                     question=self.account_attrs["datetime_format"]["help"],
                     default=self.from_file["datetime_format"]
                 )
+        #   else
+        #       set rest of parameters using default values
+            else:
+                self.port = self.from_file["port"]
+                self.ssl = self.from_file["ssl"]
+                self.username = self.from_file["username"]
+                self.datetime_format = self.from_file["datetime_format"]
         # else
         else:
         #   ask for rest of parameters
-            self.streaming_hostname = self.ask_plain(
-                header=self.account_attrs["streaming_hostname"]["help"],
-                default=self.account_attrs["streaming_hostname"]["default"]
-            )
             self.port = self.ask_plain(
                 header=self.account_attrs["port"]["help"],
                 default=self.account_attrs["port"]["default"],
@@ -98,9 +105,9 @@ class Config(object):
 
         print
         if default is None:
-            value = raw_input("{}: ".format(doc))
+            value = raw_input("{}: ".format(header))
         else:
-            value = raw_input("{} [{}]: ".format(doc, default))
+            value = raw_input("{} [{}]: ".format(header, default))
             if value == "": value = default
         if dtype is None:
             return value
@@ -109,37 +116,52 @@ class Config(object):
 
     def ask_choice(self, header, choices, question, default=None, dtype=None):
 
-        i_default = choices.index(default)+1
+        if len(choices)==1: default = choices[0]
         print
-        print ":".format(header)
+        print "{}:".format(header)
         for i, choice in enumerate(choices):
             print "[{}] {}".format(i+1, choice)
-        if default if None:
+        if default is None:
             select = raw_input("{}: ".format(question))
         else:
+            i_default = choices.index(default) + 1
             select = raw_input("{}? [{}]: ".format(question, i_default))
-        if select == "":
-            select = i_default-1
-        else:
-            select = int(select)
+            if select == "":
+                select = i_default - 1
+            else:
+                select = int(select) - 1
         if dtype is None:
             return choices[select]
         else:
             return dtype(choices[select])
 
-    def get_accounts(self, token):
-        api = oandapyV20.API(access_token=self.token)
+    def get_accounts(self, token, environment):
+        api = oandapyV20.API(access_token=token, environment=environment)
         r = AccountList()
         api.request(r)
         return [account["id"] for account in r.response["accounts"]]
 
     def get_from_file(self, filename):
-        return yaml.load(filename)
+        return yaml.load(open(filename, "r"))
+
+    def set_to_file(self, filename):
+        # with open("{}/.pycbt-config.yml".format(os.environ["HOME"]), "w") as conf_file:
+        #
+        #     conf_file.write("hostname: {}{}".format(account_info["hostname"], os.linesep))
+        #     conf_file.write("streaming_hostname: {}{}".format(account_info["streaming_hostname"], os.linesep))
+        #     conf_file.write("port: {}{}".format(account_info["port"], os.linesep))
+        #     conf_file.write("token: {}{}".format(account_info["token"], os.linesep))
+        #     conf_file.write("username: {}{}".format(account_info["username"], os.linesep))
+        #     conf_file.write("accounts:{}".format(os.linesep))
+        #     for account in account_info["accounts"]:
+        #         conf_file.write("- {}{}".format(account["id"], os.linesep))
+        #     conf_file.write("active_account: {}{}".format(account_info["active_account"], os.linesep))
+        #     conf_file.write(os.linesep)
+        return yaml.dump(open(filename, "w"))
 
     def set_info(self):
         return OrderedDict(
-            hostname=self.hostname,
-            streaming_hostname=self.streaming_hostname,
+            environment=self.environment,
             port=self.port,
             ssl=self.ssl,
             token=self.token,
