f3044bea1263de762dcd9522000b2d9ba3944590
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-04 00:11:51 -0400

    updating cbt-config script

--- cbt-config.py
+++ cbt-config.py
@@ -7,10 +7,30 @@ from copy import copy
 from collections import OrderedDict
 from pyCBT.data.providers.oanda import account
 
-# define parser
-parser = argparse.ArgumentParser()
+# DEFINE USEFUL FUNCTIONS --------------------------------------------------------------------------
+def exist(filename):
+    """Check if a given file path exist
+
+    Returns
+    -------
+        True if filename exist, False if do not.
+    """
+    try:
+        with open(filename, "r") as _file: pass
+        return True
+    except IOError:
+        return False
+
+# INITIALIZE VARIABLES -----------------------------------------------------------------------------
+# initialize comand line & file summary variables
+cmd_kwargs = OrderedDict()
+fil_kwargs = OrderedDict()
 # define config
 config = account.Config()
+
+# PARSE COMMAND LINE ARGUMENTS ---------------------------------------------------------------------
+# define parser
+parser = argparse.ArgumentParser()
 # add parameters to parser
 for arg in config.attr_names:
     # don't include accounts in parser
@@ -23,56 +43,72 @@ for arg in config.attr_names:
     # add attribute to parser
     parser.add_argument("--{}".format(arg), **kwargs)
 # add interactive option
-parser.add_argument("--interactive", help="Update config attributes interatively", action="store_true")
+parser.add_argument("--interactive", help="Update config attributes interactively", action="store_true")
 # parse arguments from command line (cmd)
 args = parser.parse_args()
+
+# UPDATE CONFIG ATTRIBUTES DEFAULT VALUES ----------------------------------------------------------
 # build cmd arguments for configuration file
-cmd_kwargs = dict(args._get_kwargs())
+cmd_kwargs.update(args._get_kwargs())
 cmd_kwargs.pop("interactive")
-# clean None values from cmd_kwargs
 cmd_kwargs = dict((arg, val) for arg, val in cmd_kwargs.iteritems() if val is not None)
-# check if 'active_account' is in command line arguments
-fil_kwargs = OrderedDict()
-if cmd_kwargs.has_key("active_account"):
-    # define filename
-    filename = config.get_filename(cmd_kwargs.get("active_account"))
-    # check if config file already exist
-    if os.path.isfile(filename):
-        response = "y"
-        if args.interactive:
-            print
-            response = raw_input("Going to load config file '{}' [Y/n]: ".format(os.path.basename(filename)))
-        if not response or response.lower().startswith("y"):
-            with open(filename, "r") as IN:
-                fil_kwargs.update(config.get_from(IN))
+# define filename choices
+filenames = [
+    config.get_filename(),
+    "Do not load from file"
+]
+if "account" in cmd_kwargs: filenames = [config.get_filename(cmd_kwargs.get("account"))] + filenames
+filenames = filter(exist, filenames)
+if args.interactive and len(filenames) > 1:
+    filename = config.ask_the_user(
+        header="Going to load config file",
+        choices=filenames,
+        default=filenames[0]
+    )
+    if filename != filenames[-1]:
+        with open(filename, "r") as IN:
+            fil_kwargs.update(config.get_from(IN))
 # update attribute defaults to file if available & command line argument values, in that order
 config.update_defaults(**fil_kwargs)
 config.update_defaults(**cmd_kwargs)
-# ask/set account attributes
-# ask if interactive or fil_kwargs is empty
-if args.interactive or not fil_kwargs:
-    config.ask_account()
+
+# SET SUMMARY ATTRIBUTES ---------------------------------------------------------------------------
+# ask/set attributes
+# ask if interactive
+if args.interactive:
     config.ask_attributes()
 else:
-    config.set_account()
+    if None in config.attr_defaults.values():
+        config.ask_attributes(only_missing=True)
     config.set_attributes()
 # set summary config
 config.set_summary()
+
+# SAVE SUMMARY(?) ----------------------------------------------------------------------------------
 # display summary
 print
 print "Summary config:"
 print
 config.dump_to(sys.stdout)
-# define filename
-filename = config.get_filename()
 # save to file?
-# default response
-response = "Y"
-print
-response = raw_input("Save summary to '{}' [Y/n]: ".format(os.path.basename(filename)))
-if response.lower().startswith("y") or response == "":
+# define filename choices
+filenames = [
+    config.get_filename(),
+    config.get_filename(config.summary["account"]),
+    "Do not save in file"
+]
+choices = map(
+    lambda f: "(replace) {}".format(f) if exist(f) else ("          {}").format(f), filenames
+)
+choice = config.ask_the_user(
+    header="Save summary in config file",
+    choices=choices,
+    default=choices[0]
+)
+filename = filenames[choices.index(choice)]
+if filename != filenames[-1]:
     with open(filename, "w") as OUT:
         config.dump_to(OUT)
-    if args.interactive:
-        print "Config file saved."
+    print
+    print "Config file saved."
 print
