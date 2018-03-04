3d24b39fc57084a8347f7aa5dbb771f643796319
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-04 14:48:51 -0400

    defining timezone_shift function & dropping original timezone functions

--- pyCBT/tools/timezone.py
+++ pyCBT/tools/timezone.py
@@ -2,27 +2,29 @@ import pytz
 from dateutil.parser import parse
 from datetime import datetime, timedelta
 
-# ERROR: join this methods
-def tz_to_utc(datetime_str=None, timezone=None):
-    """Turns a datetime string in a given timezone into a datetime string in UTC
 
-    Given 'datetime_str' in a arbitrary format and a 'timezone', this method
-    returns a version of the same datetime in the RFC3339 format.
+def timezone_shift(datetime_str=None, in_tz="America/Caracas", out_tz="UTC", fmt="RFC3339"):
+    """Turns a datetime string from one timezone to another in a given format
 
-    The given 'datetime_str' can be in any valid format to dateutil.parser.parse.
-    or in UNIX timestamp.
+    Given a datetime string in a timezone 'in_tz', this function performs the
+    conversion to 'out_tz' (only if 'in_tz' not equal to 'out_tz') and returns
+    the result in a given format 'fmt'.
 
     Parameters
     ----------
     datetime_str: str
-        a string representation of a datetime.
-
-    timezone: str
-        the name of a timezone (ex. EST, America/Caracas).
+        A string representation of a datetime.
+    in_tz, out_tz: str
+        The name of a timezone (ex. EST, America/Caracas).
+    fmt: str
+        A datetime format string (valid for datetime.strftime)
+        or one of the options:
+            - RFC3339
+            - UNIX
+        Both of this will convert to UTC regardless of 'out_tz'.
     """
-    # ERROR: implement parser as a decorator
     if datetime_str is None:
-        dt = datetime.now(tzinfo=pytz.timezone(timezone))
+        dt = datetime.now(tzinfo=pytz.timezone(in_tz))
     else:
         try:
             dt = parse(datetime_str)
@@ -31,46 +33,12 @@ def tz_to_utc(datetime_str=None, timezone=None):
                 dt = timedelta(np.float64(dt_str)) + datetime(1970, 1, 1)
             except ValueError:
                 raise ValueError("Unknown datetime format for {}.".format(datetime_str))
-    if timezone is None: timezone = "UTC"
 
-    if timezone != "UTC":
-        tz = pytz.timezone(timezone)
-        if dt.tzinfo is None: dt = dt.replace(tzinfo=tz)
-        dt = dt.astimezone(pytz.UTC)
-    return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    if dt.tzinfo is None: dt.replace(tzinfo=pytz.timezone(in_tz))
 
-def utc_to_tz(datetime_str=None, timezone=None):
-    """Turns a datetime string in UTC into a datetime string in a given timezone
+    if fmt == "RFC3339": out_tz = "UTC"
+    if in_tz != out_tz: dt.astimezone(pytz.timezone())
 
-    Given 'datetime_str' in RFC3339 format and UTC, this method returns a version
-    of the same datetime in a ISO-like format (%Y-%m-%dT%H:%M:%SZ%z).
+    dt_str = dt.strftime("%Y-%m-%dT%H:%M:%SZ" if fmt == "RFC3339" else fmt)
 
-    The given 'datetime_str' can be in any valid format to dateutil.parser.parse.
-    or in UNIX timestamp.
-
-    Parameters
-    ----------
-    datetime_str: str
-        a string representation of a datetime.
-    timezone: str
-        the name of a timezone (ex. EST, America/Caracas).
-    """
-    # ERROR: implement parser as a decorator
-    if datetime_str is None:
-        dt = datetime.now(tzinfo=pytz.timezone(timezone))
-    else:
-        try:
-            dt = parse(datetime_str)
-        except ValueError:
-            try:
-                dt = timedelta(np.float64(dt_str)) + datetime(1970, 1, 1)
-            except ValueError:
-                raise ValueError("Unknown datetime format for {}.".format(datetime_str))
-    if timezone is None: timezone = "UTC"
-
-    dt.replace(tzinfo=pytz.UTC)
-    if timezone != "UTC":
-        tz = pytz.timezone(timezone)
-        dt = dt.astimezone(tz)
-    # ERROR: implement format string as decorator to ease changing to UNIX
-    return dt.strftime("%Y-%m-%dT%H:%M:%SZ%z")
+    return dt_str
