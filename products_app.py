840a489dfaef7f3bd550e8fd38dbbe7e07a6d3e1
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-05 03:41:28 -0400

    updating products application

--- products_app.py
+++ products_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 from pyCBT.data.providers.oanda import historical
 from pyCBT.data.providers.oanda import account
@@ -27,7 +27,7 @@ for instrument in response:
             continue
 # define defaults
 default_instruments = {"candles": "WTICO_USD", "charts": ["EUR_USD", "USD_JPY", "GBP_USD", "XAU_USD"]}
-default_resolutions = {"candles": "M15", "charts": "M15"}
+default_resolutions = {"candles": "H1", "charts": "H1"}
 default_datetimes = {"candles": ("2018-02-20", "2018-03-02"), "charts": ("2018-02-20", "2018-03-02")}
 default_timezone = "America/New_York"
 
@@ -45,21 +45,42 @@ def products():
     #   get historical parameters from POST request
     #   update charts with input parameters
     #   update defaults to input parameters
-    elif request.method == "GET":
+    else:
         pass
     #   show charts for default historical values (now)
-    candles = historical.Candles(
-        instrument=default_instruments["candles"],
-        resolution=default_resolutions["candles"],
-        from_date=default_datetimes["candles"][0],
-        to_date=default_datetimes["candles"][1],
-        timezone=default_timezone
-    )
-    candles.as_dictionary()
+        candles = historical.Candles(
+            client=client,
+            instrument=default_instruments["candles"],
+            resolution=default_resolutions["candles"],
+            from_date=default_datetimes["candles"][0],
+            to_date=default_datetimes["candles"][1],
+            datetime_fmt="JSON",
+            timezone=default_timezone
+        )
+        candles = candles.as_dictionary()
+        ohlc = []
+        volume = []
+        for i in xrange(len(candles["DATETIME"])):
+            ohlc += [[
+                candles["DATETIME"][i],
+                candles["OPEN"][i],
+                candles["HIGH"][i],
+                candles["LOW"][i],
+                candles["CLOSE"][i]
+            ]]
+            volume += [[
+                candles["DATETIME"][i],
+                candles["VOLUME"][i]
+            ]]
 
     #   show the form for updating historical parameters
 
-    return render_template("products.html.j2")
+    return render_template(
+        "products.html.j2",
+        candles_symbol=instruments["name"][instruments["symbol"].index(default_instruments["candles"])],
+        ohlc=ohlc,
+        volume=volume
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
