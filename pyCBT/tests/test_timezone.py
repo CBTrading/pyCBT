c742494903af942bf65f18590e7ec3646f394d1f
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-03-11 20:40:30 -0400

    updating todos for testing module

--- pyCBT/tests/test_timezone.py
+++ pyCBT/tests/test_timezone.py
@@ -3,6 +3,25 @@ from unittest import TestCase
 import pyCBT.common.timezone as timezone
 
 class TestTimeShift(TestCase):
-    def test_is_timezone(self):
-        dt = timezone.datetime.now(tzinfo=timezone.pytz.timezone("America/Caracas"))
-        self.assertTrue(bool(dt.tzinfo))
+    tz_name = "America/Caracas"
+    dt = timezone.datetime.now(tz=timezone.pytz.timezone(tz_name))
+    dt_str_in = dt.isoformat()
+
+    dt_str_nshift = timezone.timezone_shift(
+        datetime_str=dt_str_in,
+        in_tz=tz_name,
+        out_tz=tz_name,
+        fmt="RFC3339"
+    )
+
+    # TODO: test without shifting timezone
+    # TODO:     test each fotmat (use regex to test matching strings)
+    # TODO: test with shifting timezone
+    # TODO:     test each format
+    # TODO:     test for year
+    # TODO:     test for month
+    # TODO:     test for day
+    # TODO:     test for hour
+    # TODO:     test for minutes
+    # TODO:     test for seconds
+    # TODO:     test for timezone
