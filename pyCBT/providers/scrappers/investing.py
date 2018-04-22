f6da59eea16ee3d5494e661d6828af399ab98663
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-04-21 20:47:15 -0400

    adding missing imports

--- pyCBT/providers/scrappers/investing.py
+++ pyCBT/providers/scrappers/investing.py
@@ -1,4 +1,5 @@
-import locale
+import locale, string
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
