4158afb2420b379b808a510967f78dab209a9463
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-04-22 12:43:41 -0400

    removing unstable quarter parser

--- pyCBT/common/timezone.py
+++ pyCBT/common/timezone.py
@@ -8,22 +8,10 @@ from datetime import datetime, timedelta
 #       volatility and other charts
 # TODO: implement parser as a decorator
 # TODO: implement checking if datetime_str is JSON or UNIX
-def parse_tz(datetime_str=None, in_tz="America/Caracas", remove_pattern=None, replace_pattern=None, parse_quarter=False):
+def parse_tz(datetime_str=None, in_tz="America/Caracas", remove_pattern=None, replace_pattern=None):
     if datetime_str is None:
         dt = datetime.now(tz=pytz.timezone(in_tz))
     else:
-        if parse_quarter:
-            m1 = re.findall(r"^\w{3}", datetime_str)
-            m2 = re.findall(r"\(Q\d\)", datetime_str)
-            if m2:
-                month = m1.pop()
-                _ = m2.pop()
-                imonth = datetime.strptime(month, "%b").month
-                nquart = 3 * eval(_.strip("(Q)"))
-                nmonth = imonth - nquart if nquart > 0 else 12 + imonth - nquart
-                datetime_str = datetime_str.replace(_, "")
-                datetime_str = datetime_str.replace(month, datetime(1900, nmonth, 1).strftime("%b"))
-                datetime_str = datetime_str.strip()
         if replace_pattern:
             m1 = re.findall(replace_pattern[0], datetime_str)
             m2 = re.findall(replace_pattern[1], datetime_str)
