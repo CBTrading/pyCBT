b8c150e2c2f99fbb0bd1ace9ddcc8f3280f009fc
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-05-17 23:52:36 -0400

    commenting out pattern handling from timezone module

--- pyCBT/common/timezone.py
+++ pyCBT/common/timezone.py
@@ -9,22 +9,22 @@ from datetime import datetime, timedelta
 # TODO: implement parser as a decorator
 # TODO: implement checking if datetime_str is JSON or UNIX
 # TODO: time parsing will break when replacing months with different number of days
-def parse_tz(datetime_str=None, in_tz="America/Caracas", remove_pattern=None, replace_pattern=None):
+def parse_tz(datetime_str=None, in_tz="America/New_York"):#, remove_pattern=None, replace_pattern=None):
     if datetime_str is None:
         dt = datetime.now(tz=pytz.timezone(in_tz))
     else:
-        if replace_pattern:
-            m1 = re.findall(replace_pattern[0], datetime_str)
-            m2 = re.findall(replace_pattern[1], datetime_str)
-            if m2:
-                _ = m2.pop()
-                datetime_str = datetime_str.replace(_, "")
-                datetime_str = datetime_str.replace(m1.pop(), _.strip("()"))
-                datetime_str = datetime_str.strip()
-        if remove_pattern:
-            m = re.findall(remove_pattern, datetime_str)
-            if m:
-                datetime_str = datetime_str.replace(m.pop(), "")
+        # if replace_pattern:
+        #     m1 = re.findall(replace_pattern[0], datetime_str)
+        #     m2 = re.findall(replace_pattern[1], datetime_str)
+        #     if m2:
+        #         _ = m2.pop()
+        #         datetime_str = datetime_str.replace(_, "")
+        #         datetime_str = datetime_str.replace(m1.pop(), _.strip("()"))
+        #         datetime_str = datetime_str.strip()
+        # if remove_pattern:
+        #     m = re.findall(remove_pattern, datetime_str)
+        #     if m:
+        #         datetime_str = datetime_str.replace(m.pop(), "")
         try:
             dt = parse(datetime_str)
         except ValueError:
@@ -38,7 +38,7 @@ def parse_tz(datetime_str=None, in_tz="America/Caracas", remove_pattern=None, re
     return dt
 
 # TODO: check if need to parse or if datetime is already a datetime object
-def timezone_shift(datetime_str=None, in_tz="America/Caracas", out_tz="UTC", fmt="RFC3339", remove_pattern=None, replace_pattern=None):
+def timezone_shift(datetime_str=None, in_tz="America/New_York", out_tz="UTC", fmt="RFC3339"):#, remove_pattern=None, replace_pattern=None):
     """Turns a datetime string from one timezone to another in a given format
 
     Given a datetime string in a timezone 'in_tz', this function performs the
@@ -59,7 +59,7 @@ def timezone_shift(datetime_str=None, in_tz="America/Caracas", out_tz="UTC", fmt
             - JSON
         Any the listed options will be in UTC regardless of 'out_tz'.
     """
-    dt = parse_tz(datetime_str, in_tz, remove_pattern, replace_pattern)
+    dt = parse_tz(datetime_str, in_tz)#, remove_pattern, replace_pattern)
     if fmt in ["RFC3339", "UNIX", "JSON"]: out_tz = "UTC"
     if in_tz != out_tz: dt = dt.astimezone(pytz.timezone(out_tz))
 
