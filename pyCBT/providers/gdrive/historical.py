fb981e680c7ff0d6fddffcb6d101b9b5dbd74280
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2018-06-19 20:02:02 -0400

    updating gdrive historical module

--- pyCBT/providers/gdrive/historical.py
+++ pyCBT/providers/gdrive/historical.py
@@ -4,7 +4,7 @@ import string
 import itertools as it
 import pickle as pk
 
-from talib import MA, EMA, WMA, RSI, CCI, ROC, MOM, WILLR
+from talib.abstract import MA, EMA, WMA, RSI, CCI, ROC, MOM, WILLR
 
 from pyCBT.providers.gdrive.account import get_client
 from pyCBT.common.path import exist
@@ -34,37 +34,36 @@ class DriveTables(object):
             self.filenames[file.get("title")] = file.get("id")
 
         # initialize object attributes
-        self.symbols = symbols
-        self.reference = reference
+        self.all_symbols = symbols
+        self.ref_symbol = reference
         # download tables
         if exist(self.PICKLE):
-            self.reference_table, self.symbols_table = pk.load(open(self.PICKLE, "rb"))
+            self.ref_dataframe, self.all_dataframes = pk.load(open(self.PICKLE, "rb"))
         else:
-            self.reference_table = self._download_reference_symbol()
-            self.symbols_table = self._download_other_symbols()
-            pk.dump((self.reference_table, self.symbols_table), open(self.PICKLE, "wb"))
+            self.ref_dataframe = self._download_ref_data()
+            self.all_dataframes = self._download_all_data()
+            pk.dump((self.ref_dataframe, self.all_dataframes), open(self.PICKLE, "wb"))
 
-        # initialize features
-        self._features_params = None
-        self.features = None
+        # initialize joint dataframe
+        self.joint_dataframe = None
 
     def _get_filename(self, symbol):
         """Returns filename corresponding to given symbol."""
-        if self.symbols.get(symbol)["category"] == "economic-calendar":
-            return "{}.csv".format(self.symbols.get(symbol)["instrument"])
+        if self.all_symbols.get(symbol)["category"] == "economic-calendar":
+            return "{}.csv".format(self.all_symbols.get(symbol)["instrument"])
         else:
-            return "{}-{}.csv".format(self.resolution, self.symbols.get(symbol)["instrument"])
+            return "{}-{}.csv".format(self.resolution, self.all_symbols.get(symbol)["instrument"])
 
     def _get_category(self, symbol):
         """Returns category of given symbol."""
-        category = self.symbols.get(symbol)["category"]
+        category = self.all_symbols.get(symbol)["category"]
         category = category.replace("-", " ")
         category = category.capitalize()
         return category
 
-    def _download_reference_symbol(self):
+    def _download_ref_data(self):
         """Downloads reference symbol"""
-        _id = self.filenames.get(self._get_filename(self.reference))
+        _id = self.filenames.get(self._get_filename(self.ref_symbol))
         reference_table = pd.read_csv(
             filepath_or_buffer=io.StringIO(self.client.CreateFile({"id":_id}).GetContentString().decode("utf-8")),
             index_col="Date",
@@ -72,74 +71,98 @@ class DriveTables(object):
         )
         return reference_table
 
-    def _download_other_symbols(self):
+    def _download_all_data(self):
         """Downloads other symbols from symbols' dictionary."""
         tables = {}
-        for symbol in self.symbols:
+        for symbol in self.all_symbols:
             print "downloading... \t{}".format(symbol)
             _id = self.filenames.get(self._get_filename(symbol))
-            if symbol == self.reference:
-                tables[symbol] = self.reference_table.get("Close")
+            if symbol == self.ref_symbol:
+                tables[symbol] = self.ref_dataframe.get("Close")
             else:
                 tables[symbol] = pd.read_csv(
                     filepath_or_buffer=io.StringIO(self.client.CreateFile({"id":_id}).GetContentString().decode("utf-8")),
                     index_col="Date",
                     parse_dates=True,
-                    usecols=("Date", ("Actual" if self.symbols.get(symbol)["category"] == "economic-calendar" else "Close"),)
+                    usecols=("Date", ("Actual" if self.all_symbols.get(symbol)["category"] == "economic-calendar" else "Close"),)
                 )
         return tables
 
-    # TODO: split this method into three: get_technical, get_prices, get_returns
-    def get_features(self, lag_periods=1, ti_periods=5):
-        """Returns features table."""
+    def get_technical(self, indicators={"EMA": EMA, "MA": MA, "WMA": WMA, "%R": WILLR, "CCI": CCI, "MOM": MOM, "ROC": ROC, "RSI": RSI}, periods=5):
+        """Returns technical indicators for reference symbol."""
+        # make a copy of reference table to handle TaLib functions
+        df = self.ref_dataframe.copy()
+        df.rename(str.lower, axis="columns", inplace=True)
 
-        # check if features exist with same parameters
-        if self.features is not None and self._features_params == (lag_periods, ti_periods): return self.features
-        # define features parameters
-        self._features_params = (lag_periods, ti_periods)
-        # copy reference index to build features table
-        ref_index = self.reference_table.index.copy()
-        # extract columns from raw data
+        table = pd.DataFrame(
+            index=self.ref_dataframe.index.copy(),
+            columns=pd.MultiIndex.from_tuples(
+                tuples=list(it.product(["Technical"], sorted(indicators.keys()))),
+                names=["Category", "Name"]
+            ),
+            data=None
+        )
+        for name in sorted(indicators):
+            table["Technical", name] = indicators[name](df, timeperiod=periods)
+
+        return table
+
+    def get_joint_dataframe(self):
+        """Returns joint table with all symbols."""
+        if self.joint_dataframe is not None: return self.joint_dataframe
         # initialize columns list
         tables = []
-        for category in set(map(lambda s: self.symbols.get(s)["category"], self.symbols)):
+        for category in set(map(lambda s: self.all_symbols.get(s)["category"], self.all_symbols)):
             category_name = category.replace("-", " ").capitalize()
             # extract symbols of current catagory
-            cat_symbols = filter(lambda s: self.symbols.get(s)["category"] == category, self.symbols)
-            # define lag for given category (economic calendars will not be lagged)
-            lag = lag_periods if category != "economical-calendar" else 0
+            cat_symbols = sorted(filter(lambda s: self.all_symbols.get(s)["category"] == category, self.all_symbols))
             # fill tables list with symbols of current category
             table = pd.DataFrame(
-                index=ref_index,
+                index=self.ref_dataframe.index.copy(),
                 columns=pd.MultiIndex.from_tuples(
                     tuples=list(it.product([category_name], cat_symbols)),
                     names=["Category", "Name"]
                 ),
                 data=None
             )
-            for symbol in cat_symbols: table[category_name, symbol] = self.symbols_table.get(symbol).shift(lag)
+            for symbol in cat_symbols: table[category_name, symbol] = self.all_dataframes.get(symbol)
             tables += [table]
 
         # add to tables list technical indicators from reference symbol
-        table = pd.DataFrame(
-            index=ref_index,
-            columns=pd.MultiIndex.from_tuples(
-                tuples=list(it.product(["Technical"], ["EMA", "MA", "WMA", "%R", "CCI", "MOM", "ROC", "RSI"])),
-                names=["Category", "Name"]
-            ),
-            data=None
-        )
-        table["Technical", "EMA"] = EMA(self.reference_table.Close.shift(lag), ti_periods)
-        table["Technical", "MA"] = MA(self.reference_table.Close.shift(lag), ti_periods, matype=0)
-        table["Technical", "WMA"] = WMA(self.reference_table.Close.shift(lag), ti_periods)
-        table["Technical", "%R"] = WILLR(self.reference_table.High.shift(lag), self.reference_table.Low.shift(lag), self.reference_table.Close.shift(lag), ti_periods)
-        table["Technical", "CCI"] = CCI(self.reference_table.High.shift(lag), self.reference_table.Low.shift(lag), self.reference_table.Close.shift(lag), ti_periods)
-        table["Technical", "MOM"] = MOM(self.reference_table.Close.shift(lag), ti_periods)
-        table["Technical", "ROC"] = ROC(self.reference_table.Close.shift(lag), ti_periods)
-        table["Technical", "RSI"] = RSI(self.reference_table.Close.shift(lag), ti_periods)
-        tables += [table]
-        # define features
-        self.features = pd.concat(tables, axis="columns")
-        self.features.dropna(how="all", axis="index", inplace=True)
-        self.features.interpolate(method="time", limit_direction="both", inplace=True)
-        return self.features
+        tables += [self.get_technical()]
+        # define joint dataframe
+        self.joint_dataframe = pd.concat(tables, axis="columns")
+        self.joint_dataframe.dropna(how="all", axis="index", inplace=True)
+        self.joint_dataframe.interpolate(method="time", limit_direction="both", inplace=True)
+        return self.joint_dataframe
+
+    def get_prices(self):
+        """Returns prices for the financial instruments."""
+        if self.joint_dataframe is None: self.get_joint_dataframe()
+
+        symbols = filter(lambda s: self.all_symbols.get(s)["category"] != "economic-calendar", self.all_symbols)
+        symbols = map(lambda s: (self._get_category(s), s), symbols)
+
+        df = self.joint_dataframe.filter(items=symbols)
+        return df
+
+    def get_returns(self):
+        """Returns fraction of change for the financial instruments."""
+        if self.joint_dataframe is None: self.get_joint_dataframe()
+
+        symbols = filter(lambda s: self.all_symbols.get(s)["category"] != "economic-calendar", self.all_symbols)
+        symbols = map(lambda s: (self._get_category(s), s), symbols)
+
+        df = self.joint_dataframe.filter(items=symbols)
+        df = df.pct_change()
+        return df
+
+    def get_economical(self):
+        """Returns economic indicators."""
+        if self.joint_dataframe is None: self.get_joint_dataframe()
+
+        symbols = filter(lambda s: self.all_symbols.get(s)["category"] == "economic-calendar", self.all_symbols)
+        symbols = map(lambda s: (self._get_category(s), s), symbols)
+
+        df = self.joint_dataframe.filter(items=symbols)
+        return df
